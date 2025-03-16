1d636ce428e9767cba012eb2a62363852a7494c6
HendriADS <hendri@ayodyads.com>
2025-03-17 06:04:24 +0700

    test push after ceonfig the ssh

--- main.dart
+++ main.dart
@@ -1,8 +1,15 @@
 void main(List<String> args) {
   final sum = sumNumber(10, 90);
+  final checkBoolean = isTrue();
+
   print(sum);
+  print(checkBoolean);
 }
 
 int sumNumber(int a, int b) {
   return a + b;
+}
+
+bool isTrue() {
+  return 'hello' == 12;
 }
